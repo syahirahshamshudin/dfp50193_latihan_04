a5c31e295a5166dba851216362e22558cd9c8d0f
syahirahshamshudin <sharifahsyahirah1351@gmail.com>
2021-10-07 09:50:04 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+define('JAWATAN','Pengarah');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constants</title>
+    <style>
+        p{font-size: 24px; font-family: Verdana, Geneva ,Verdana, Geneva, Tahoma, sans-serif;}
+    </style>
+
+</head>
+<body>
+    <p>
+    Saya selaku <?php echo JAWATAN; ?>
+    dengan ini melantik Encik Ahmad sebagai 
+    Penolong <?php echo JAWATAN; ?>
+    </p>
+</body>
+</html>
